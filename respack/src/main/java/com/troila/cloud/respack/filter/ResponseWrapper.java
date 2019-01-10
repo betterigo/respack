5fb59bf7b106c19ae3c96d76b9762a39e14899f2
haodonglei <haodonglei@troila.com>
2019-01-10 17:58:44 +0800

    12
    1

--- respack/src/main/java/com/troila/cloud/respack/filter/ResponseWrapper.java
+++ respack/src/main/java/com/troila/cloud/respack/filter/ResponseWrapper.java
@@ -2,7 +2,6 @@ package com.troila.cloud.respack.filter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
@@ -18,15 +17,21 @@ public class ResponseWrapper extends HttpServletResponseWrapper {
     private ByteArrayOutputStream bytes = new ByteArrayOutputStream();
     private PrintWriter printWriter;
     private ServletOutputStream filterOutput;
-    
+    private ServletOutputStream servletOutputStream;
+    private boolean hasWriteBytes = false;
     
     public ResponseWrapper(HttpServletResponse response) {
         super(response);
+        try {
+			this.servletOutputStream = response.getOutputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
     
     @Override
     public ServletOutputStream getOutputStream() throws IOException {
-    	filterOutput = new MyServletOutputStream(bytes); 
+    	filterOutput = new MyServletOutputStream(bytes,servletOutputStream); 
     	return filterOutput;
     }
   
@@ -56,20 +61,55 @@ public class ResponseWrapper extends HttpServletResponseWrapper {
         return bytes.toByteArray();
     }
     
-    public void writeFileStream(OutputStream out) {
-    	
+    public void writeFileStream(HttpServletResponse response) throws IOException {
+//    	response.addIntHeader("Content-Length", bytes.size());
+//    	ServletOutputStream os = response.getOutputStream();
+//    	bytes.writeTo(os);
+//    	os.w
+//    	this.servletOutputStream.
+    	this.servletOutputStream.flush();
     }
 
     class MyServletOutputStream extends ServletOutputStream {
+    	
+    	private static final int TOTAL_SIZE = 1 * 1024 * 1024;//1m的json字符串长度限制
+    	
         private ByteArrayOutputStream os ;
 
+        private ServletOutputStream servletOutputStream;
+        
         public MyServletOutputStream(ByteArrayOutputStream ostream) {
             this.os = ostream;
         }
 
-        @Override
+        
+        
+		public MyServletOutputStream(ServletOutputStream servletOutputStream) {
+			super();
+			this.servletOutputStream = servletOutputStream;
+		}
+
+		public MyServletOutputStream(ByteArrayOutputStream os, ServletOutputStream servletOutputStream) {
+			super();
+			this.os = os;
+			this.servletOutputStream = servletOutputStream;
+		}
+
+
+
+		@Override
         public void write(int b) throws IOException {
-            os.write(b);
+			//应该设定os最大读取的字节数。避免oom
+			if(os.size()<TOTAL_SIZE) {				
+				os.write(b);//此处需要判断是否是json字符串,如果不是json，那么也需要直接使用servletOutputStream输出
+			}else {
+				//超过最大阈值会直接输出
+				if(!hasWriteBytes) {					
+					servletOutputStream.write(os.toByteArray(), 0, os.size());
+					hasWriteBytes = true;
+				}
+				servletOutputStream.write(b);
+			}
         }
 
 		@Override
