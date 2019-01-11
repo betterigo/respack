40e5892bd45b4042ba70b60e66951f42d0848427
haodonglei <haodonglei@troila.com>
2019-01-11 09:44:52 +0800

    添加/error路径解析

---
+++ respack/src/main/java/com/troila/cloud/respack/core/ErrorBody.java
@@ -0,0 +1,42 @@
+package com.troila.cloud.respack.core;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ErrorBody {
+	private int status;
+	@JsonFormat(timezone="GMT+8")
+	private Date timeStamp;
+	private String message;
+	private String path;
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	@Override
+	public String toString() {
+		return "ErrorBody [status=" + status + ", timestamp=" + timeStamp + ", message=" + message + ", path=" + path
+				+ "]";
+	}
+}
